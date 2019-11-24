d6092238a9fd3389610c6f4f366f3283557629bb
jsharp4 <jasharp2000@gmail.com>
2019-11-24 15:47:52 -0600

    added code for testing fft sink problem

---
+++ fft_test.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+##################################################
+# GNU Radio Python Flow Graph
+# Title: Top Block
+# Generated: Sun Nov 24 15:43:21 2019
+##################################################
+
+
+if __name__ == '__main__':
+    import ctypes
+    import sys
+    if sys.platform.startswith('linux'):
+        try:
+            x11 = ctypes.cdll.LoadLibrary('libX11.so')
+            x11.XInitThreads()
+        except:
+            print "Warning: failed to XInitThreads()"
+
+from gnuradio import audio
+from gnuradio import blocks
+from gnuradio import eng_notation
+from gnuradio import gr
+from gnuradio import wxgui
+from gnuradio.eng_option import eng_option
+from gnuradio.fft import window
+from gnuradio.filter import firdes
+from gnuradio.wxgui import fftsink2
+from grc_gnuradio import wxgui as grc_wxgui
+from optparse import OptionParser
+import wx
+
+
+class top_block(grc_wxgui.top_block_gui):
+
+    def __init__(self):
+        grc_wxgui.top_block_gui.__init__(self, title="Top Block")
+        _icon_path = "/usr/share/icons/hicolor/32x32/apps/gnuradio-grc.png"
+        self.SetIcon(wx.Icon(_icon_path, wx.BITMAP_TYPE_ANY))
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.samp_rate = samp_rate = 44100
+
+        ##################################################
+        # Blocks
+        ##################################################
+        self.wxgui_fftsink2_0 = fftsink2.fft_sink_f(
+        	self.GetWin(),
+        	baseband_freq=0,
+        	y_per_div=5,
+        	y_divs=10,
+        	ref_level=0,
+        	ref_scale=2.0,
+        	sample_rate=samp_rate,
+        	fft_size=10240,
+        	fft_rate=15,
+        	average=False,
+        	avg_alpha=None,
+        	title='FFT Plot',
+        	peak_hold=False,
+        )
+        self.Add(self.wxgui_fftsink2_0.win)
+        self.blocks_wavfile_source_1 = blocks.wavfile_source('/home/jsarp4/Documents/gnu_radio_flowcharts/CSS_USRP_impl/chirps/1.wav', True)
+        self.blocks_wavfile_source_0 = blocks.wavfile_source('/home/jsarp4/Documents/gnu_radio_flowcharts/CSS_USRP_impl/chirps/inv.wav', True)
+        self.blocks_multiply_xx_0 = blocks.multiply_vff(1)
+        self.audio_sink_1 = audio.sink(samp_rate, '', True)
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.connect((self.blocks_multiply_xx_0, 0), (self.audio_sink_1, 0))
+        self.connect((self.blocks_multiply_xx_0, 0), (self.wxgui_fftsink2_0, 0))
+        self.connect((self.blocks_wavfile_source_0, 0), (self.blocks_multiply_xx_0, 1))
+        self.connect((self.blocks_wavfile_source_1, 0), (self.blocks_multiply_xx_0, 0))
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+        self.wxgui_fftsink2_0.set_sample_rate(self.samp_rate)
+
+
+def main(top_block_cls=top_block, options=None):
+
+    tb = top_block_cls()
+    tb.Start(True)
+    tb.Wait()
+
+
+if __name__ == '__main__':
+    main()
